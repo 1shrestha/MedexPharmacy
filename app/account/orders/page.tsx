ce09e64b52fc84b515a66a1204b07e39dd402048
Shrestha pandey <pandeyshree141@gmail.com>
2026-09-08 21:38:03 +0530

    Update page.tsx

--- app/account/orders/page.tsx
+++ app/account/orders/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+ export const dynamic = "force-dynamic";
 import { useEffect, useState } from "react";
 import { useSession, signIn } from "next-auth/react";
 import Link from "next/link";
